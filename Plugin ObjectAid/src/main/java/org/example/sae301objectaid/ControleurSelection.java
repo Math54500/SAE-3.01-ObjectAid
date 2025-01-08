cdb7e93d7c2a9f5912461559ecbb20d760e3f44f
Math54500 <Maths54500z@gmail.com>
2025-01-08 08:20:38 +0100

    modification ControleurSelection 3

--- Plugin ObjectAid/src/main/java/org/example/sae301objectaid/ControleurSelection.java
+++ Plugin ObjectAid/src/main/java/org/example/sae301objectaid/ControleurSelection.java
@@ -3,8 +3,10 @@ package org.example.sae301objectaid;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.TreeView;
+import javafx.scene.input.MouseEvent;
 
-public class ControleurSelection implements EventHandler<ActionEvent> {
+public class ControleurSelection implements EventHandler<MouseEvent> {
 
     private Model model;
 
@@ -13,9 +15,11 @@ public class ControleurSelection implements EventHandler<ActionEvent> {
     }
 
     @Override
-    public void handle(ActionEvent e) {
+    public void handle(MouseEvent m) {
 
-        System.out.println("poutre de castor");
+        TreeView<String> treeView = (TreeView<String>) m.getSource();
+        System.out.println(treeView.getSelectionModel().getSelectedItem().getValue());
 
     }
+
 }
